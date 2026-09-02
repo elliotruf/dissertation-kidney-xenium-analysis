4717fff3922676fa793198fae9d7c4a7817a2159
Elliot Ruf <RufE@cardiff.ac.uk>
2026-09-02 17:34:48 +0100

    Updated helper function versions, corrected duplicate error with assign_broad_types().

--- scripts/R_scripts/helpers/project_paths_v3.R
+++
@@ -1,9 +0,0 @@
-# =====================================
-# Project paths
-# =====================================
-
-project_root <- getwd()
-
-project_path <- function(...) {
-  file.path(project_root, ...)
-}
